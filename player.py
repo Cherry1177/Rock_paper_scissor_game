9912306dbf1c0036a2418ae7e0eedb3199e08ec8
cherry <ayethihan01@gmail.com>
2026-02-20 22:43:04 +0800

    add prompt for users typo errors

--- player.py
+++ player.py
@@ -1,8 +1,13 @@
 def get_user_input(valid_choices):
     user_choice = input("Enter rock, paper, or scissors: ").lower()
 
+    while(user_choice not in valid_choices):
+        print("Oops! That's not a valid move. Please enter rock, paper, or scissors.")
+        user_choice = input("Enter rock, paper, or scissors: ").lower()
+        
     if user_choice in valid_choices:
         return user_choice
-    else:
-        print("Oops! That's not a valid move.")
-        return None
+        
+    # else:
+    #     print("Oops! That's not a valid move.")
+    #     return None
