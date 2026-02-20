f38c64b640a9f0b9b2e55e031982b1c04f7054ed
cherry <ayethihan01@gmail.com>
2026-02-20 23:12:47 +0800

    added score for player and computer

--- main.py
+++ main.py
@@ -9,7 +9,6 @@ from game_logic import determine_winner
 from player import get_user_input
 from computer import get_computer_choice
 
-play_again = "yes"
 
 def get_emoji(choice):
     return EMOJI_MAP.get(choice)
@@ -22,23 +21,28 @@ welcome_user()
 
 valid_choices =['rock', 'paper', 'scissors']
 
-def round (play_again):
+player_score = 0
+computer_score = 0
+play_again = "yes"
+
+while play_again == "yes":
     user_choice = get_user_input(valid_choices)
-    computer_choice = get_computer_choice(valid_choices)
-    result = determine_winner(user_choice, computer_choice)
     print(f"You chose: {get_emoji(user_choice)}")
+    computer_choice = get_computer_choice(valid_choices)
     print(f"Computer chose: {get_emoji(computer_choice)}")
+    result = determine_winner(user_choice, computer_choice)
     print(result)
-
-round(play_again)
-
-play_again = input("Do you want to play again? (yes/no): ")
-
-while(play_again == "yes"):
-    round(play_again)
+    if(result == "You win!"):
+        player_score = player_score + 1
+    elif(result == "You lose!"):
+        computer_score = computer_score + 1
+    else:
+        player_score += 0
+        computer_score += 0
+    print(f"Your score: {player_score}")
+    print(f"Computer score: {computer_score}")
     play_again = input("Do you want to play again? (yes/no): ")
 
-while(play_again == "no"):
-    print("Thank you for playing")
-    exit()
+print("Thank you for playing")
+
     
