5632bcc1cc1a9afdd96041ba167d7b1b0d07595d
cherry <ayethihan01@gmail.com>
2026-02-20 22:27:05 +0800

    added replay loop

--- main.py
+++ main.py
@@ -4,9 +4,12 @@ EMOJI_MAP = {
    "scissors" : "✌️"
 }
 
+import random
 from game_logic import determine_winner
 from player import get_user_input
 from computer import get_computer_choice
+
+play_again = "yes"
 
 def get_emoji(choice):
     return EMOJI_MAP.get(choice)
@@ -19,12 +22,23 @@ welcome_user()
 
 valid_choices =['rock', 'paper', 'scissors']
 
-user_choice = get_user_input(valid_choices)
+def round (play_again):
+    user_choice = get_user_input(valid_choices)
+    computer_choice = get_computer_choice(valid_choices)
+    result = determine_winner(user_choice, computer_choice)
+    print(f"You chose: {get_emoji(user_choice)}")
+    print(f"Computer chose: {get_emoji(computer_choice)}")
+    print(result)
 
-computer_choice = get_computer_choice(valid_choices)
+round(play_again)
 
-result = determine_winner(user_choice, computer_choice)
+play_again = input("Do you want to play again? (yes/no): ")
 
-print(f"You chose: {get_emoji(user_choice)}")
-print(f"Computer chose: {get_emoji(computer_choice)}")
-print(result)
+while(play_again == "yes"):
+    round(play_again)
+    play_again = input("Do you want to play again? (yes/no): ")
+
+while(play_again == "no"):
+    print("Thank you for playing")
+    exit()
+    
